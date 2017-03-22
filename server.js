b5d180fcb6515471f64c4a37dd1e53b86b551d25
anaya30 <anaya.arfin3001@gmail.com>
2017-03-22 20:34:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto= require('crypto');
 var app = express();
 app.use(morgan('combined'));
 
@@ -62,6 +62,18 @@ function createTemplate (data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+function hash(input) {
+    var hashed=crypto.pbkdf2Sync(input, salt,10000,512,'sha512');
+    return hashed.toString(hex);
+}
+
+
+app.get('/hash/:input',function(req,res) {
+   var hashedString=hash(req.params.input 'This is some random strin');
+   res.send(hashedString);
 });
 
 app.get('/article-one',function(req,res){
