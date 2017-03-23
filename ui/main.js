ed2903c0a4bdb9f96619577a3f788b39c49118b0
anaya30 <anaya.arfin3001@gmail.com>
2017-03-23 19:12:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,14 @@
-console.log('Loaded!');
-var element = document.getElementById('main text');
-var img = document.getElementById('madi');
-img.onclick= function () {
-    img.style.marginLeft='100px';
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
+
+button.onclick= function() {
+    //make a request to the counter variable
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in the correct span
+    counter=counter+1;
+    var span=documnent.getElementById('count');
+    span.innerhtml=counter.toString();
 };
