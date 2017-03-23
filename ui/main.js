22ddc366eff6ba7bf931d55ac857fcfab98a93ab
anaya30 <anaya.arfin3001@gmail.com>
2017-03-23 20:08:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,21 +2,24 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
-    //make a request to the counter variable
+    //create a request object
     var request = new XMLHttpRequest();
     
-    
-    
-    //capture the response and store it in a variable
+      //capture the response and store it in a variable
     request.onreadystatechange = function() {
         if(request.readystate===XMLHttpRequest.DONE)
         {
             if(request.status===200) {
             var counter =  request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+            
             }
         }
-    //render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
+        
+    };    
+    //make the request
+    request.open('GET',' http://anaya30.imad.hasura-app.io/counter', true);
+    request.send(null);
+
 };
