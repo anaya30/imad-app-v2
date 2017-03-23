91dd163658aa9cddbc22bf4d9240be2a91a618c6
anaya30 <anaya.arfin3001@gmail.com>
2017-03-23 19:13:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 //counter code
 var button=document.getElementById('counter');
 var counter=0;
-
 button.onclick= function() {
     //make a request to the counter variable
     
@@ -10,5 +9,5 @@ button.onclick= function() {
     //render the variable in the correct span
     counter=counter+1;
     var span=documnent.getElementById('count');
-    span.innerhtml=counter.toString();
+    span.innerHTML=counter.toString();
 };
