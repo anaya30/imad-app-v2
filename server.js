a5922f05bd044380cb341817788b7b267d44cff2
anaya30 <anaya.arfin3001@gmail.com>
2017-03-24 21:44:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name/:name',function(req,res){
     //get the name the request
-    var name = req.params.name; //1000
+    var name = req.params.name; //TODO
     names.push(name);
     //JSON:Javascript Object Notation
      res.send(JSON.stringify(names));
